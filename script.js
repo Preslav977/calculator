6d1102e68b6e5481b2efc3da1e6f55ec3e247abf
Preslav977 <p.cvetanow@gmail.com>
2023-01-13 11:42:43 +0200

    Added design to the calculator

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //variables
 let displayCalculator = document.getElementById("calculator-display");
-let clearBtn = document.getElementById("clear-btn");
+const clearBtn = document.getElementById("clear-btn");
 let backspaceBtn = document.getElementById("backspace-btn");
 const sevenBtn = document.getElementById("seven-btn");
 const eightBtn = document.getElementById("eight-btn");
@@ -19,71 +19,176 @@ const dotBtn = document.getElementById("dot-btn");
 let equalBtn = document.getElementById("equal-btn");
 let divideBtn = document.getElementById("divide-btn");
 
-firstNumber = "";
-secondNumber = "";
-operator = "";
+var operandOne = "";
+var operandTwo = "";
+var operator = "";
 
-function displayCalculatorValues(operandOne, operator, operandTwo) {
-    displayCalculator.textContent = `${operandOne}${operator}${operandTwo}`;
+function displayNumberZero() {
+  displayCalculator.textContent = "0";
 }
 
-function saveNumber(operand) {
-  if(operator) {
-    //if exist 
-    operand += secondNumber;
-   //add it to the secondNumber and return it
-   return operand;
-  } else {
-    //if it doesn't exist
-    operand += firstNumber;
-    //add it to the firstNumber and return it
-    return operand;
-  }
+function displayNumberOne() {
+  displayCalculator.textContent += "1";
 }
 
-// zeroBtn.addEventListener('click', function () {
-//   console.log(saveNumber("0"));
-//   console.log(displayCalculatorValues("0", operator, "0"));
-// });
+function displayNumberTwo() {
+  displayCalculator.textContent += "2";
+ 
+ }
+ 
+ function displayNumberThree() {
+  displayCalculator.textContent += "3";
+ 
+ }
 
-oneBtn.addEventListener('click', function() {
-  console.log(saveNumber("1"));
-  console.log(displayCalculatorValues("1", operator, "1"));
+ function displayNumberFour() {
+  displayCalculator.textContent += "4";
+ 
+ }
+ 
+ function displayNumberFive() {
+  displayCalculator.textContent += "5";
+ 
+ }
+
+ function displayNumberSix() {
+  displayCalculator.textContent += "6";
+ 
+ }
+ 
+ function displayNumberSeven() {
+   displayCalculator.textContent += "7";
+ 
+ }
+
+ function displayNumberEight() {
+  displayCalculator.textContent += "8";
+ 
+ }
+ 
+ function displayNumberNine() {
+  displayCalculator.textContent += "9";
+ 
+ }
+
+zeroBtn.addEventListener('click', function(){
+  console.log(displayNumberZero());
 });
 
+oneBtn.addEventListener('click', function() {
+  console.log(displayNumberOne());
+});
 
+twoBtn.addEventListener('click', function(){
+  console.log(displayNumberTwo());
+});
 
-//operate function that checks if the operator
-//has some of the symbols and base on condition
-//calls the methods and returns the result
+threeBtn.addEventListener('click', function() {
+  console.log(displayNumberThree());
+});
 
+fourBtn.addEventListener('click', function(){
+  console.log(displayNumberFour());
+});
+
+fiveBtn.addEventListener('click', function() {
+  console.log(displayNumberFive());
+});
+
+sixBtn.addEventListener('click', function(){
+  console.log(displayNumberSix());
+});
+
+sevenBtn.addEventListener('click', function() {
+  console.log(displayNumberSeven());
+});
+
+eightBtn.addEventListener('click', function(){
+  console.log(displayNumberEight());
+});
+
+nineBtn.addEventListener('click', function() {
+  console.log(displayNumberNine());
+});
+
+addBtn.addEventListener('click', function() {
+  operandOne = displayCalculator.textContent;
+  console.log(operandOne);
+  displayCalculator.textContent += "+";
+  operator = displayCalculator.textContent;
+  console.log(operator);
+  operandTwo = displayCalculator.textContent;
+  console.log(operandTwo);
+});
+
+subtractBtn.addEventListener('click', function() {
+  operandOne = displayCalculator.textContent;
+  console.log(operandOne);
+  displayCalculator.textContent += "-";
+  operator = displayCalculator.textContent;
+  console.log(operator);
+  operandTwo = displayCalculator.textContent;
+  console.log(operandTwo);
+});
+
+multiplyBtn.addEventListener('click', function() {
+  operandOne = displayCalculator.textContent;
+  console.log(operandOne);
+  displayCalculator.textContent += "*";
+  operator = displayCalculator.textContent;
+  console.log(operator);
+  operandTwo = displayCalculator.textContent;
+  console.log(operandTwo);
+});
+
+divideBtn.addEventListener('click', function() {
+  operandOne = displayCalculator.textContent;
+  console.log(operandOne);
+  displayCalculator.textContent += "/";
+  operator = displayCalculator.textContent;
+  console.log(operator);
+  operandTwo = displayCalculator.textContent;
+  console.log(operandTwo);
+});
+
+equalBtn.addEventListener('click', function() {
+  console.log(operate(operandOne, operator, operandTwo))
+});
+
+//operate function that calls one of the four calculate
+//functions based on operator as string
 function operate(operandOne, operator, operandTwo) {
-  if (operator === "+") {
-    return add(operandOne, operandTwo);
+  // let saveOperation;
+  if(operator === "+") {
+    // saveOperation = operator;
+    return addOperands(operandOne, operandTwo);
   } else if (operator === "-") {
-    return subtract(operandOne, operandTwo);
+    // saveOperation = operator;
+    return subtractOperands(operandOne, operandTwo);
   } else if (operator === "*") {
-    return multiply(operandOne, operandTwo);
+    // saveOperation = operator;
+    return multiplyOperands(operandOne, operandTwo);
   } else if (operator === "/") {
-    return divide(operandOne, operandTwo);
+    // saveOperation = operator;
+    return divideOperands(operandOne, operandTwo);
   }
+  // saveOperation = operator;
 }
 
-//function that does the simply adding,
-//subtracting, multiplying, dividing
-
-function add(operandOne, operandTwo) {
+//functions that calculate the four simple operations 
+//such as +, -, *, /
+function addOperands(operandOne, operandTwo) {
   return Number(operandOne + operandTwo);
 }
 
-function subtract(operandOne, operandTwo) {
+function subtractOperands(operandOne, operandTwo) {
   return Number(operandOne - operandTwo);
 }
 
-function multiply(operandOne, operandTwo) {
+function multiplyOperands(operandOne, operandTwo) {
   return Number(operandOne * operandTwo);
 }
 
-function divide(operandOne, operandTwo) {
+function divideOperands(operandOne, operandTwo) {
   return Number(operandOne / operandTwo);
 }
