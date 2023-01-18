f4c37eeddc6fec6b016c3e3e0f8113b71b9ff0de
Preslav977 <p.cvetanow@gmail.com>
2023-01-18 09:25:32 +0200

    Made the calculator to save the first operand, operator and second operand

--- script.js
+++ script.js
@@ -1,42 +1,51 @@
-//operator variables
-let displayCalculator = document.getElementById("calculator-display");
-const dotBtn = document.getElementById("dot-btn");
-let equalBtn = document.getElementById("equal-btn");
-let backspaceBtn = document.getElementById("backspace-btn");
+//variables
+const displayCalculator = document.getElementById("calculator-display");
+const equalBtn = document.getElementById("equal-btn");
+const backspaceBtn = document.getElementById("backspace-btn");
 const clearBtn = document.getElementById("clear-btn");
 
 //selecting all numbers with the class bth-number
 //looping through each one with forEach
 //adding eventListener click with callback function
 const calculateNumbers = document.querySelectorAll('.btn-number');
+
+function displayNumbers(e) {
+  displayCalculator.textContent += e.target.textContent;
+  displayValue = displayCalculator.textContent;
+  // console.log(displayValue);
+}
+
 calculateNumbers.forEach(btn => {
   btn.addEventListener('click', displayNumbers);
-  function displayNumbers(e) {
-    displayCalculator.textContent += e.target.textContent;
-  };
 });
 
-//selecting all operators with class bth-operator
-//looping through each one with forEach
-//adding eventListener click with callback function
+let displayValue = "";
+let operandOne = "";
+let operator = "";
+let operandTwo = "";
+
 const calculatorOperators = document.querySelectorAll('.btn-operator');
+
+function showCalculations(e) {
+  console.log(e.target.textContent);
+  if(!operandOne) {
+    operandOne = displayCalculator.textContent;
+    console.log(operandOne);
+    operator = e.target.textContent;
+    console.log(operator);
+    displayCalculator.textContent = "";
+  } else if (!operandTwo) {
+    operandTwo = displayValue;
+    console.log(operandTwo);
+  }
+}
+
 calculatorOperators.forEach(operator => {
   operator.addEventListener('click', showCalculations);
-  function showCalculations(e) {
-    displayCalculator.textContent += e.target.textContent;
-  };
 });
 
-
-let operandOne = "";
-let operandTwo = "";
-let operator = "";
-
-
 equalBtn.addEventListener('click', function() {
-  // operandTwo = displayCalculator.textContent;
-  // console.log(operandTwo);
-  // console.log(operate("2", "+", "2"))
+  console.log(operate(operandOne, operator, operandTwo));
 });
 
 clearBtn.addEventListener('click', function () {
@@ -45,36 +54,21 @@ clearBtn.addEventListener('click', function () {
   }
 });
 
-backspaceBtn.addEventListener('click', function () {
-
-});
-
-// dotBtn.addEventListener('click', function () {
-//   if(displayCalculator.textContent === "") {
-//     console.log("ERROR");
-//   } else {
-//     displayCalculator.textContent += ".";
-//   }
+// backspaceBtn.addEventListener('click', function () {
 // });
 
 //operate function that calls one of the four calculate
 //functions based on operator as string
 function operate(operandOne, operator, operandTwo) {
-  // let saveOperation;
   if(operator === "+") {
-    // saveOperation = operator;
     return addOperands(operandOne, operandTwo);
   } else if (operator === "-") {
-    // saveOperation = operator;
     return subtractOperands(operandOne, operandTwo);
   } else if (operator === "*") {
-    // saveOperation = operator;
     return multiplyOperands(operandOne, operandTwo);
   } else if (operator === "/") {
-    // saveOperation = operator;
     return divideOperands(operandOne, operandTwo);
   }
-  // saveOperation = operator;
 }
 
 //functions that calculate the four simple operations 
